67327e7e4b2712810abf02f47bd21569a078e69c
Meeta Kapoor <47625349+jupdec@users.noreply.github.com>
2024-02-12 19:22:27 -0800

    CS646 Assignment 1
    Code base for Pizza Party Application

---
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.zybooks.pizzaparty
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.ceil
+
+const val SLICES_PER_PIZZA = 8
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var numAttendEditText: EditText
+    private lateinit var numPizzasTextView: TextView
+    private lateinit var howHungryRadioGroup: RadioGroup
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
+        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+    }
+
+    fun calculateClick(view: View) {
+
+ /**
+  * Get the value as typed by the user
+  * into theEditText
+  */
+
+        val numAttendStr = numAttendEditText.text.toString()
+
+/**
+ * Convert that text
+ * into an integer*/
+
+        val numAttend = numAttendStr.toInt()
+
+/**
+ * Determine the number of slices on an
+ * average each person will eat*/
+
+        val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
+            R.id.light_radio_button -> 2
+            R.id.medium_radio_button -> 3
+            else -> 4
+        }
+
+/**
+ * Calculate and show
+ * the number of pizzas needed*/
+
+        val totalPizzas = ceil(numAttend * slicesPerPerson / SLICES_PER_PIZZA.toDouble()).toInt()
+        numPizzasTextView.text = "Total pizzas: $totalPizzas"
+    }
+}
